77e07a1e9cb110e39929fd6ae5ad27b3ab301c67
yeahbah <yeahbah@outlook.com>
2016-03-10 15:28:30 -0700

    meh

---
+++ PSEGetLib/Service/AmibrokerService.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Diagnostics;
+using LateBindingHelper;
+using PSEGetLib.Interfaces;
+
+namespace PSEGetLib.Service
+{
+    public class AmibrokerService : IAmibrokerService
+    {
+        public bool IsAmibrokerInstalled()
+        {
+            try
+            {
+
+                Process[] localByName = Process.GetProcessesByName("broker");
+                if (localByName.Any())
+                {
+                    return true;
+                }
+                else
+                {
+                    IOperationInvoker amiInvoker = BindingFactory.CreateAutomationBinding("Broker.Application");
+                    try
+                    {
+                        return amiInvoker != null;
+                    }
+                    finally
+                    {
+                        if (amiInvoker != null)
+                            amiInvoker.Method("Quit").Invoke();
+                    }
+                }                
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
