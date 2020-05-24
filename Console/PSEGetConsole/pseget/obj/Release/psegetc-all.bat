055fed7a5836edcfb954c1c1d211bc8c04cea448
Anthony Ganzon <anthony.ganzon@ymail.com>
2020-05-24 23:30:18 +0800

    Added a batch file to run executable on windows

---
+++ Console/PSEGetConsole/pseget/obj/Release/psegetc-all.bat
@@ -0,0 +1,3 @@
+@echo off
+
+psegetc -t https://www.pse.com.ph/resource/dailyquotationreport/file/ -df 01/01/2019 -dt 01/10/2019 -o C:\Users\ACER-4755G\Documents\Tony\Projects\github\PSEGet\Console\PSEGetConsole\pseget\obj\Release\Output
