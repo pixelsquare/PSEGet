9503f735ecc9a06a1f02b3f341171a8a7c0cd3fd
Anthony Ganzon <anthony.ganzon@ymail.com>
2020-05-28 16:22:21 +0800

    Added psegetc command to convert downloaded files to amibroker data

--- Console/PSEGetConsole/pseget/obj/Release/psegetc-all.bat
+++ Console/PSEGetConsole/pseget/obj/Release/psegetc-all.bat
@@ -1,3 +1,4 @@
 @echo off
 
-psegetc -t https://www.pse.com.ph/resource/dailyquotationreport/file/ -df 01/01/2019 -dt 01/10/2019 -o C:\Users\ACER-4755G\Documents\Tony\Projects\github\PSEGet\Console\PSEGetConsole\pseget\obj\Release\Output
+::psegetc -t https://www.pse.com.ph/resource/dailyquotationreport/file/ -df 12/20/2019 -dt 05/22/2020 -o C:\Users\ACER-4755G\Documents\Tony\Projects\github\PSEGet\Console\PSEGetConsole\pseget\obj\Release\Output
+psegetc -t https://www.pse.com.ph/resource/dailyquotationreport/file/ -df 12/01/2016 -dt 06/01/2017 -f ami:"C:\Program Files (x86)\AmiBroker\Data"
